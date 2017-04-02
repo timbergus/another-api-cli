3fc1459a5efd72dd83929fda0ad496da1af67e09
Gustavo Muñoz <timbergus@gmail.com>
2017-04-02 18:10:17 +0200

    Add web sockets.

--- modules/api/templates/sockets.js
+++ modules/api/templates/sockets.js
@@ -5,6 +5,7 @@ module.exports = listener => {
   const io = new Io(listener);
 
   io.on('connection', socket => {
-    console.log(chalk.green.bgWhite(`Socket ${ socket } connected!`));
+    console.log(chalk.green.bgWhite(`Socket ${ socket.id } connected!`));
+    socket.on('isAlive', () => socket.emit('alive'));
   });
 };
